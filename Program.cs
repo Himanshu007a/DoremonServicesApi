96752bf73dc5f5d5e35e65d3280e89c76f2ca28c
shubham <shubham>
2023-07-25 15:54:59 +0530

    inta

--- Program.cs
+++ Program.cs
@@ -1,8 +1,13 @@
+using DoremonServicesApi.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("defaultConnection")
 
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
